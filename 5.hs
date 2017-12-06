ae9eb6cb186b8739c4d615e8cf5008fb6d91a433
Frerich Raabe <raabe@froglogic.com>
2017-12-06 18:56:35 +0100

    Simplified

--- 5.hs
+++ 5.hs
@@ -1,27 +1,25 @@
 import qualified Data.Vector.Unboxed as V
 import Data.List (unfoldr)
+import Data.Maybe (catMaybes, isJust)
 
-data Memory = Memory Int (V.Vector Int) deriving Show
+type Memory = (Int, V.Vector Int)
 
 parse :: String -> Memory
-parse s = Memory 0 (V.fromList . map read . lines $ s)
+parse s = (0, V.fromList . map read . lines $ s)
 
-step :: (Int -> Int) -> Memory -> Maybe Memory
-step adjust (Memory pos mem) =
+step :: (Int -> Int) -> Memory -> Memory
+step adjust (pos, mem) =
     if val < 0 && pos + val >= 0 || val >= 0 && pos + val < V.length mem
-      then Just $ Memory (pos + val) (mem V.// [(pos, adjust val)])
-      else Nothing
+      then (pos + val, mem V.// [(pos, adjust val)])
+      else ((-1), V.empty)
   where
     val = mem V.! pos
 
-walk :: (Int -> Int) -> Memory -> [Memory]
-walk adjust m = m : unfoldr (\x -> case step adjust x of Just m' -> Just (m', m'); Nothing -> Nothing) m
-
 partOne :: String -> Int
-partOne = length . walk (+1) . parse
+partOne = length . takeWhile (not . V.null . snd) . iterate (step (+1)) . parse
 
 partTwo :: String -> Int
-partTwo = length . walk (\x -> if x >= 3 then x - 1 else x + 1) . parse
+partTwo = length . takeWhile (not . V.null . snd) . iterate (step (\x -> if x >= 3 then x - 1 else x + 1)) . parse
 
 main :: IO ()
-main = interact $ show . partOne
+main = interact $ show . partOne . parse
