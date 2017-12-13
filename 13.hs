d75f9f893c02d46e4092f6d2908cc9c1d714bb90
Frerich Raabe <raabe@froglogic.com>
2017-12-13 18:09:52 +0100

    Optimized today's solution
    Avoid recalculting length in collisionsAt, drop usage of Data.Map, fold
    frequency.

--- 13.hs
+++ 13.hs
@@ -1,34 +1,30 @@
-import qualified Data.Map.Strict as Map
-
-frequency :: Int -> Int
-frequency 1 = 1
-frequency n = 2 * (n - 1)
+import Data.Array (listArray, elems, (//))
 
 topRowState :: [Int] -> Int -> [Int]
 topRowState ranges picoSec = map stateAt ranges
   where
     stateAt 0 = 0
-    stateAt range = if picoSec `mod` frequency range == 0 then 1 else 0
+    stateAt range = if picoSec `mod` (2 * (range - 1)) == 0 then 1 else 0
 
-collisionsAt :: [Int] -> Int -> [Int]
-collisionsAt ranges picoSec = zipWith (!!) states [0..length ranges - 1]
+collisionsAt :: Int -> [Int] -> Int -> [Int]
+collisionsAt len ranges picoSec = zipWith (!!) states [0..len - 1]
    where
-     states = map (topRowState ranges) [picoSec..picoSec + length ranges]
+     states = map (topRowState ranges) [picoSec..picoSec + len]
 
 parse :: String -> [Int]
-parse s = map (\i -> Map.findWithDefault 0 i parsedMap) [0..maximum (Map.keys parsedMap)]
+parse s = elems (listArray (0, maximum (map fst parsedMap)) (repeat 0) // parsedMap)
   where
-    parsedMap = Map.fromList . map parseLine . lines $ s
+    parsedMap = map parseLine (lines s)
     parseLine l = let [d, r] = words l in (read (init d), read r)
 
 partOne :: [Int] -> Int
 partOne ranges = sum (zipWith (*) collisions severities)
   where
-    collisions = collisionsAt ranges 0
+    collisions = collisionsAt (length ranges) ranges 0
     severities = zipWith (*) [0..] ranges
 
 partTwo :: [Int] -> Int
-partTwo ranges = head . filter (all (== 0) . collisionsAt ranges) $ [0..]
+partTwo ranges = head . filter (all (== 0) . collisionsAt (length ranges) ranges) $ [0..]
 
 main :: IO ()
 main = do
