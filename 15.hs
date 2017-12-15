26b0fdf8979f57082b23494a51ebc7a9f871b28b
Frerich Raabe <raabe@froglogic.com>
2017-12-15 10:51:54 +0100

    Optimised today's solution
    It used to run in 28 seconds, but only when printing both solutions; if
    I print either partOne or partTwo, it was just about two seconds. Some
    discussion on #haskell revealed that the global 'sequence*' lists might
    be the cause, and indeed folding them helped.
    
    This new version now runs in 2.8 seconds.

--- 15.hs
+++ 15.hs
@@ -1,27 +1,20 @@
 import Data.Bits ((.&.))
 import Data.Function (on)
-import Prelude hiding (sequenceA)
 
 generator :: Int -> Int -> Int
 generator factor input = (input * factor) `rem` 2147483647
 
-run :: (Int -> Int) -> Int -> [Int]
-run g = drop 1 . iterate g
-
-sequenceA :: [Int]
-sequenceA = run (generator 16807) 883
-
-sequenceB :: [Int]
-sequenceB = run (generator 48271) 879
-
-equalPairs :: Int -> [Int] -> [Int] -> Int
-equalPairs numPairs seqA seqB = length . filter id . take numPairs $ zipWith ((==) `on` (.&. 0xffff)) seqA seqB
+equalPairs :: Int -> (Int -> Bool) -> (Int -> Bool) -> Int
+equalPairs numPairs predA predB = length . filter id . take numPairs $ zipWith ((==) `on` (.&. 0xffff)) (filter predA seqA) (filter predB seqB)
+  where
+    seqA = iterate (generator 16807) 883
+    seqB = iterate (generator 48271) 879
 
 partOne :: Int
-partOne = equalPairs 40000000 sequenceA sequenceB
+partOne = equalPairs 40000000 (const True) (const True)
 
 partTwo :: Int
-partTwo = equalPairs 5000000 (filter (divides 4) sequenceA) (filter (divides 8) sequenceB)
+partTwo = equalPairs 5000000 (divides 4) (divides 8)
   where
     divides a b = b `rem` a == 0
 
