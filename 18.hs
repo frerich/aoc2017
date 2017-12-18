73ee53212f727b29f6661ecced924250bf0ab643
Frerich Raabe <raabe@froglogic.com>
2017-12-18 23:06:29 +0100

    Simplify

--- 18.hs
+++ 18.hs
@@ -31,7 +31,7 @@ data SoundAction
 type Program = Vector Instruction
 type Memory = Map Char Int
 type Queue = Seq Int
-data Machine = Machine Queue Int Memory
+data Process = Process Queue Int Memory
 
 parseProgram :: String -> Program
 parseProgram = either (error . show) id . parse program ""
@@ -53,58 +53,56 @@ parseProgram = either (error . show) id . parse program ""
         decimal <- read <$> many1 digit
         return (sign decimal)
 
-eval :: Instruction -> (Char -> Machine -> (Machine, Maybe SoundAction)) -> Machine -> (Machine, Maybe SoundAction)
-eval instr rcvHandler (Machine input ip mem) =
+brokenEval :: Instruction -> Process -> (Process, Maybe SoundAction)
+brokenEval instr process@(Process input ip mem) =
     case instr of
-        Snd x   -> (Machine input (ip + 1) mem, Just (Sound (value x)))
-        Set x y -> (Machine input (ip + 1) (Map.insert x (value y)                          mem), Nothing)
-        Add x y -> (Machine input (ip + 1) (Map.insert x (value (Register x) + value y)     mem), Nothing)
-        Mul x y -> (Machine input (ip + 1) (Map.insert x (value (Register x) * value y)     mem), Nothing)
-        Mod x y -> (Machine input (ip + 1) (Map.insert x (value (Register x) `rem` value y) mem), Nothing)
-        Rcv x   -> rcvHandler x (Machine input ip mem)
+        Rcv x -> if Map.findWithDefault 0 x mem /= 0
+                  then (Process input (ip + 1) mem, Just (Recover (Seq.index input (Seq.length input - 1))))
+                  else (Process input (ip + 1) mem, Nothing)
+        _     -> eval instr process
+
+eval :: Instruction -> Process -> (Process, Maybe SoundAction)
+eval instr (Process input ip mem) =
+    case instr of
+        Snd x   -> (Process input (ip + 1) mem, Just (Sound (value x)))
+        Set x y -> (Process input (ip + 1) (Map.insert x (value y)                          mem), Nothing)
+        Add x y -> (Process input (ip + 1) (Map.insert x (value (Register x) + value y)     mem), Nothing)
+        Mul x y -> (Process input (ip + 1) (Map.insert x (value (Register x) * value y)     mem), Nothing)
+        Mod x y -> (Process input (ip + 1) (Map.insert x (value (Register x) `rem` value y) mem), Nothing)
+        Rcv x   -> if Seq.null input
+                    then (Process input ip mem, Nothing)
+                    else (Process (Seq.drop 1 input) (ip + 1) (Map.insert x (Seq.index input 0) mem), Just (Recover 0))
         Jgz x y -> if value x > 0
-                    then (Machine input (ip + value y) mem, Nothing)
-                    else (Machine input (ip + 1) mem, Nothing)
+                    then (Process input (ip + value y) mem, Nothing)
+                    else (Process input (ip + 1) mem, Nothing)
   where
     value (Register r) = Map.findWithDefault 0 r mem
     value (Number x)   = x
 
 partOne :: Program -> Int
-partOne program = go (Machine Seq.empty 0 Map.empty)
+partOne program = go (Process Seq.empty 0 Map.empty)
   where
-    go machine@(Machine input ip mem) =
-        case eval (program Vector.! ip) rcvHandler machine of
-            (machine'               , Nothing)          -> go machine'
-            (Machine input' ip' mem', Just (Sound x))   -> go (Machine (input Seq.|> x) ip' mem')
+    go process@(Process input ip mem) =
+        case brokenEval (program Vector.! ip) process of
+            (process'               , Nothing)          -> go process'
+            (Process input' ip' mem', Just (Sound x))   -> go (Process (input Seq.|> x) ip' mem')
             (_                      , Just (Recover x)) -> x
-
-    rcvHandler x (Machine input ip mem) =
-        if Map.findWithDefault 0 x mem /= 0
-            then (Machine input (ip + 1) mem, Just (Recover (Seq.index input (Seq.length input - 1))))
-            else (Machine input (ip + 1) mem, Nothing)
 
 partTwo :: Program -> Int
 partTwo program = go (processA, id) (processB, (+1)) 0
   where
-    go (pa@(Machine a_input a_ip a_mem), fa) (pb@(Machine b_input b_ip b_mem), fb) cnt
+    go (pa@(Process a_input a_ip a_mem), fa) (pb@(Process b_input b_ip b_mem), fb) cnt
         | suspended pa && suspended pb = cnt
         | otherwise =
-        case eval (program Vector.! a_ip) rcvHandler pa of
+        case eval (program Vector.! a_ip) pa of
             (pa', Nothing)          -> go (pb, fb) (pa', fa) cnt
-            (pa', Just (Sound x))   -> go (Machine (b_input Seq.|> x) b_ip b_mem, fb) (pa', fa) (fa cnt)
+            (pa', Just (Sound x))   -> go (Process (b_input Seq.|> x) b_ip b_mem, fb) (pa', fa) (fa cnt)
             (pa', Just (Recover x)) -> go (pb, fb) (pa', fa) cnt
 
-    rcvHandler x (Machine input ip mem) =
-        if Seq.null input
-            then (Machine input ip mem, Nothing)
-            else (Machine (Seq.drop 1 input) (ip + 1) (Map.insert x (Seq.index input 0) mem), Just (Recover 0))
-
-    suspended :: Machine -> Bool
-    suspended m@(Machine _ ip _) = let (Machine _ ip' _, _) = eval (program Vector.! ip) rcvHandler m
+    suspended m@(Process _ ip _) = let (Process _ ip' _, _) = eval (program Vector.! ip) m
                                    in ip == ip'
-
-    processA = Machine Seq.empty 0 Map.empty
-    processB = Machine Seq.empty 0 (Map.singleton 'p' 1)
+    processA = Process Seq.empty 0 Map.empty
+    processB = Process Seq.empty 0 (Map.singleton 'p' 1)
 
 main :: IO ()
 main = do
